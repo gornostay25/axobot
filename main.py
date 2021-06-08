fc4a7cb98d5baa7a38d281a75fa6abdda8ec24b0
Gornostay <36516130+gornostay25@users.noreply.github.com>
2021-06-08 20:06:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from telethon import TelegramClient
-import time,requests,re,os,random
+from telethon import TelegramClient,events
+import time,requests,re,os,random,asyncio
 axo_bot = "AXOClaimBot"
 api_id= 2376043
 api_hash= "e992d55690981f66c95ce87695434296"
 
 ocr_api = str(os.sys.argv[1]) if len(os.sys.argv) > 2 else "0748b1c15b88957"
 
-
+isActive = False
 client = TelegramClient(
     'axo_bot_'+str(os.sys.argv[2]) if len(os.sys.argv) > 3 else "docker",
     api_id, 
@@ -15,6 +15,21 @@ client = TelegramClient(
 print(ocr_api)
 client.start()
 print("start app")
+
+@client.on(events.NewMessage(outgoing=True, pattern='!bot_(s|a|d)'))
+async def handler(event):
+    global isActive
+    if event._chat.username == axo_bot:
+        if re.findall(r'_s',event.message.message):
+            m = await event.respond('Bot is '+("active" if isActive else "deactivated"))
+        elif re.findall(r'_a',event.message.message):
+            m = await event.respond('Bot is activated! \nPlease whait 5 min')
+            isActive = True
+        elif re.findall(r'_d',event.message.message):
+            m = await event.respond('Bot is deactivated! \nPlease whait 5 min')
+            isActive = False
+        await asyncio.sleep(5)
+        await client.delete_messages(event.chat_id, [event.id, m.id])
 
 
 async def get_messages(client):
@@ -55,15 +70,15 @@ async def ocrible(mess,client):
                 await client.send_message(axo_bot, r.text)
                 print("error ocr")
                 os.remove(path)
-                time.sleep(2*60)
+                await asyncio.sleep(2*60)
                 continue
         await client.send_message(axo_bot, re.findall(r'\d+',r)[0])
-        time.sleep(2)
+        await asyncio.sleep(2)
         mess = await get_messages(client)
         if not mess:
             print("bot whaiting 30min")
-            time.sleep(30*60)
-            return
+            await asyncio.sleep(30*60)
+            return None
             
         if mess.photo:
             continue
@@ -71,25 +86,29 @@ async def ocrible(mess,client):
             print("+ 1$")
             print("sleep 16min")
             os.remove(path)
-            time.sleep(16*60)
+            await asyncio.sleep(16*60)
             return
 
 async def main():
     while 1:
         try:
+            if not isActive:
+                print("sleep no active")
+                await asyncio.sleep(5*60)
+                continue
             mess = await get_messages(client)
             if not mess:
                 print("bot whaiting 30min")
-                time.sleep(30*60)
+                await asyncio.sleep(30*60)
             print("- if")
             if not re.match(r'captcha',mess.message):
                 print("- asq captcha")
                 await client.send_message(axo_bot, '💵Claim')
-                time.sleep(5)
+                await asyncio.sleep(5)
                 mess = await get_messages(client)
                 if not mess:
                     print("bot whaiting 30min")
-                    time.sleep(30*60)
+                    await asyncio.sleep(30*60)
                     continue
                 print("-"*30)
                 print(mess.message.encode('ascii',"ignore").decode())
@@ -100,7 +119,7 @@ async def main():
                     await ocrible(mess,client)
                 else:
                     print("no photo!!!/sleep 3min")
-                    time.sleep(3*60)
+                    await asyncio.sleep(3*60)
                 
             elif (mess.photo):
                 print("- yes captcha")
